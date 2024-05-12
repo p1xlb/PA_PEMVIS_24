6bc1871866c1549b1fca632df5414e80a39e3e0f
N. Belva <tempakkul@gmail.com>
2024-05-12 11:47:09 +0800

    Login

--- PA_PEMVIS_2/Form1.vb
+++ PA_PEMVIS_2/Form1.vb
@@ -1,16 +1,27 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         Dim username As String = txtUsername.Text
         Dim password As String = txtPassword.Text
-        If username = "admin" And password = "admin" Then
-            MsgBox("Logged in As Admin")
+        CMD = New MySqlCommand("SELECT * FROM pelanggan WHERE username = '" & username & "' AND password = '" & password & "'", CONN)
+        RD = CMD.ExecuteReader
+        RD.Read()
+        If RD.HasRows Then
+            MsgBox("Login Berhasil")
+            Me.Hide()
+            UserDash.Show()
+        ElseIf username = "Admin" And password = "Admin" Then
+            MsgBox("Login Berhasil")
             Me.Hide()
             AdminDash.Show()
-
-
-
         Else
             MsgBox("Login Gagal")
         End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        koneksi()
     End Sub
 End Class
